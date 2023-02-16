35803faa0f1c5542c5555e517a9f7f0eaf4c0cad
Erzenkell <ferrier.sammy@gmail.com>
2023-02-16 12:08:10 +0100

    Added subtext on input

--- src/components/Input/TextInput.js
+++ src/components/Input/TextInput.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./TextInput.css";
 
-const TextInput = ({label, placeholder, type}) => {
+const TextInput = ({label, placeholder, type, subtext}) => {
     return (
         <div className="text_input_container">
             <label className="text_input_label">{label}</label>
             <input type={type} className="text_input" placeholder={placeholder}/>
+            <div className="text_input_subtext">{subtext}</div>
         </div>
     )
 }
